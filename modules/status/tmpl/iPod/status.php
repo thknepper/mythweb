97c3fc1f9c400ccd72a7bf59d267d8b82f573a3a
Thomas Knepper <thknepper@gmx.de>
2010-08-31 23:02:14 +0200

    iPod-Style for status page

--- modules/status/tmpl/iPod/status.php
+++ modules/status/tmpl/iPod/status.php
@@ -18,10 +18,13 @@
 
 // Custom headers
     $headers[] = '<link rel="stylesheet" type="text/css" href="'.skin_url.'/ListPanel.css">';
+    $headers[] = '<link rel="stylesheet" type="text/css" href="'.skin_url.'/status.css">';
+
 
  // Setup some vars for the ipod template
     $Page_Previous_Location = root_url;
     $Page_Previous_Location_Name = 'Start';
+    $Page_Title_Short = 'Status';
 
 // Print the page header
     require 'modules/_shared/tmpl/'.tmpl.'/header.php';
